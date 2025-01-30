7558991fc1c88c4ebb956bb73ba0f5361efed2a8
Narramula Prabhakar <prabhakarnarramula@gmail.com>
2025-01-30 11:03:31 +0530

    Add files via upload
    practice program files

---
+++ neww.js
@@ -0,0 +1,10 @@
+let i=1;
+let space=5-;
+while(i<=5){
+     let spaces = ' '
+     console.log(spaces.repeat(space-i)+ "* ".repeat(i));
+     i++;
+}
+     
+   
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
